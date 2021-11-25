ea134a70f29d723efa81189f3d69515512541941
Lucas <lucas.senoville@outlook.fr>
2021-11-25 16:03:44 +0100

    migrating backend from js to ts

--- code/web/server/routes/posts.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getPosts } from '../controllers/posts.js';
-const router = express.Router();
-
-router.get('/', getPosts);
-
-//fichier exporté pour être utiliser dans index.js 
-export default router;
