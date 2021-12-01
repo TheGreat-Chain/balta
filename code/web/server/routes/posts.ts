c335e94fa9f351294c600549123c7d18dcf0db4a
Lucas <lucas.senoville@outlook.fr>
2021-12-01 13:29:05 +0100

    début set up pour graphql

--- code/web/server/routes/posts.ts
+++ code/web/server/routes/posts.ts
@@ -4,5 +4,4 @@ const router = express.Router();
 
 router.get('/', getPosts);
 
-//fichier exporté pour être utiliser dans index.js 
 export default router;
--- code/web/server/src/index.ts
+++ code/web/server/src/index.ts
@@ -1,19 +1,42 @@
-import express, {Request, Response, NextFunction} from "express";
+import express, {Request, Response, NextFunction, request} from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import graphql, { graphqlHTTP } from "express-graphql";
+import {buildSchema} from 'graphql';
+import dotenv from 'dotenv';
 import cors from "cors";
 import postRoutes from "../routes/posts";
+import { Console } from "console";
+import { schema } from "../api/schema";
+import { rootCertificates } from "tls";
 const app = express();
 
-app.use('/', postRoutes);
 
+
+//toutes les requêtes reçues vont être formatées en json
+app.use(bodyParser.json());
+
+dotenv.config();
+
+//TODO faire le cors
 app.use(cors());
-//TODO : METTRE LES IDENTIFIANTS DANS UN FICHIER .env
-const CONNECTION_URL : string = 'mongodb+srv://lucas:Y6cP9ebPhU34ygYC@lucascluster.by24p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT || 5000;
+//Ne pas oublier ! à la fin => cela permet de rendre le type en string uniquement et non string ou null
+const CONNECTION_URL : string = process.env.CONNECTION_URL!;
+const PORT = process.env.PORT;
 
+
+//connection à la base de données
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT,() => console.log(`Server running on port : ${PORT}`)))
     .catch((error : Error) => console.log("Message d'erreur : " + error.message));
     
+//graphql middleware
+app.use(process.env.GRAPHQL_PATH!,graphqlHTTP((request, response, graphQLparams) => ({
+    schema = schema,
+    rootValue = root,
+    graphiql = true,
+    context = {
+        const packageName = require('packageName');
+    }
+)}));
