5288a385ce9b2a576f9619f8ce1dfc3a35821687
Lucas <lucas.senoville@outlook.fr>
2022-05-19 13:56:15 +0200

    début branche creation_qcm

--- web/server/controllers/queries.ts
+++ web/server/controllers/queries.ts
@@ -1,12 +1,27 @@
-import { Request, Response , NextFunction} from "express"; 
+import {Request, Response, NextFunction, RequestHandler} from "express";
 import users from "../models/users";
 import mongoose from "mongoose";
 import bcryptjs, {hash} from "bcryptjs";
 import signJWT from "../functions/signJWT";
+import nodemailer from "nodemailer";
+
+let testAccount = nodemailer.createTestAccount();
+
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        //user: testAccount.user, // generated ethereal user
+        //pass: testAccount.pass, // generated ethereal password
+    },
+});
 
 
 
-const login = (req: Request, res: Response, next: NextFunction) => {
+
+const login : RequestHandler = (req: Request, res: Response, next: NextFunction) => {
     let { username, password } = req.body;
 
     users.find({ username })
@@ -49,7 +64,7 @@ const login = (req: Request, res: Response, next: NextFunction) => {
         });
 };
 
-const getAllUsers =  (req : Request, res : Response, next : NextFunction) => {
+const getAllUsers : RequestHandler =  (req : Request, res : Response, next : NextFunction) => {
     users.find()
     .exec()
     .then(results => {
@@ -66,11 +81,24 @@ const getAllUsers =  (req : Request, res : Response, next : NextFunction) => {
     });
 };
 
-const validateToken = (req: Request, res: Response, next: NextFunction) => {
+const validateToken : RequestHandler = (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json({
         message: 'Token(s) validated'
     });
 };
 
+const sendForgotPassword : RequestHandler = async(req: Request, res: Response, next: NextFunction) => {
+    const {email} : {email : String}= req.body;
+    /*try{
+        const user = await users.findOne({email});
+        if(!user) return res.status(404).json({
+            message : 'Email not valid'
+        });
+        const encryptedToken = await bcryptjs.hash(user.id.toString(),8);
+
+        let mail = transporter.sendMail
+    }*/
+
+};
 
 export default {getAllUsers, login};
