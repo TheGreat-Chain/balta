89f432db45d0cc1b48cd9b6da74474c77707f62f
DE COLIGNY Thibault <59777006+MunHaxx@users.noreply.github.com>
2022-06-10 18:49:20 +0200

    Ajout de ListeDesNotes

--- web/client/src/Connected/ContentConnected.js
+++ web/client/src/Connected/ContentConnected.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Accueil from './Components/Accueil.js';
 import MesCorrections from './Components/Correction/MesCorrections.js';
 import MesQCM from './Components/QCM/MesQCM.js';
+import ListeDesNotes from './Components/Correction/ListeDesNotes.js';
 
 import './ContentConnected.css'
 
@@ -13,8 +14,10 @@ function ContentNotConnected () {
                 <Route exact path="/" element={<Accueil />} />
 
                 <Route path="/mes-corrections" element={<MesCorrections />} />
-
-                <Route path="/mes-QCM" element={<MesQCM />} />
+                    
+                <Route path="/liste-des-notes" element={<ListeDesNotes />} />
+                    
+                <Route path="/mes-QCM" element={<ListeQCM />} />
 
                 {/* <Route path="/password-forget" element={<PasswordForget />} /> */}
             </Routes>
